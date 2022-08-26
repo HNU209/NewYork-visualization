848ace4aee0c6f62dd4acc45702c837b4f3bf81d
happy956 <happy8604@naver.com>
2022-08-26 09:24:07 +0900

    .

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import Splash from './component/Splash';
+import Main from './component/Main';
+import './css/app.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Main/>
+    </div>
+  );
+}
