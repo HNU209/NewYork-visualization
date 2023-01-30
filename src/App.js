f4db67c99662ef33b5a35b513750fe52a392af35
happy956 <happy8604@naver.com>
2023-01-30 12:35:31 +0900

    update all

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import "mapbox-gl/dist/mapbox-gl.css";
+import React, { useState, useEffect, useCallback } from "react";
+import axios from "axios";
+
+import Splash from "./components/Splash";
+import Trip from "./components/Trip";
+import "./css/app.css";
+
+const fetchData = (FilE_NAME) => {
+//   const res = axios.get(
+//     `https://raw.githubusercontent.com/HNU209/NewYork-visualization/main/src/data/${FilE_NAME}.json`
+//   );
+  const res = axios.get(
+    `data/${FilE_NAME}.json`
+  );
+  const data = res.then((r) => r.data);
+  return data;
+};
+
+const App = () => {
+  const [trip, setTrip] = useState([]);
+  const [emptyTaxi, setEmptyTaxi] = useState([]);
+  const [passenger, setPassenger] = useState([]);
+  const [isloaded, setIsLoaded] = useState(false);
+
+  const getData = useCallback(async () => {
+    const TRIP = await fetchData("trip");
+    const EMPTY_TAXI = await fetchData("empty_taxi");
+    const PASSENGER = await fetchData("ps_location");
+
+    setTrip((prev) => TRIP);
+    setEmptyTaxi((prev) => EMPTY_TAXI);
+    setPassenger((prev) => PASSENGER);
+    setIsLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    getData();
+  }, [getData]);
+
+  return (
+    <div className="container">
+      {!isloaded && <Splash />}
+      {isloaded && (
+        <Trip trip={trip} emptyTaxi={emptyTaxi} passenger={passenger} />
+      )}
+    </div>
+  );
+};
+
+export default App;
